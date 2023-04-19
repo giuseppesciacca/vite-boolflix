614146e474276707af5ea0130c3f4ff78eb3f136
giuseppesciacca <sciaccagi@gmail.com>
2023-04-19 12:15:24 +0200

    [add] fetchmovie() on store.js + print results-/M1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script>
-export default {
+import { store } from './store.js'
 
+export default {
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+    this.store.fetchMovies(this.store.urlBase + this.store.movie);
+  },
 }
 </script>
 
-<template></template>
+<template>
+  <h1>Looking for 'Mario'...</h1>
+  <ol>
+    <li class="my-3" v-for="result in this.store.results">
+      <ul>
+        <li><strong>Title: {{ result.title }}</strong></li>
+        <li>Original Title: {{ result.original_title }}</li>
+        <li>Language: {{ result.original_language }}</li>
+        <li>Vote: {{ result.vote_average }}/10</li>
+      </ul>
+    </li>
+  </ol>
+</template>
 
 <style lang="scss"></style>
---
+++ src/store.js
@@ -0,0 +1,21 @@
+import { reactive } from 'vue'
+import axios from 'axios'
+
+export const store = reactive({
+    urlBase: 'https://api.themoviedb.org/3/search/movie?api_key=aa7ff67870b167647ae7e4a3d1b611a3&query=',
+    movie: 'mario',
+    results: [],
+    fetchMovies(url) {
+        axios
+            .get(url)
+            .then(response => {
+                //console.log(response.data);
+                console.log(response.data.results);
+                this.results = response.data.results;
+
+
+            }).catch(error => {
+                console.error(error.message);
+            })
+    }
+})
