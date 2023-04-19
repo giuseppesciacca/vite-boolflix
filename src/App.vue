08488c90469e804280b2340247e2dbc3fdae3598
giuseppesciacca <sciaccagi@gmail.com>
2023-04-19 16:45:38 +0200

    [add] fetchTvShows() in the same input /End M2

--- src/App.vue
+++ src/App.vue
@@ -12,27 +12,27 @@ export default {
   methods: {
     searchMovie() {
       //console.log(this.movie);
-      this.store.fetchMovies(this.store.urlBase + this.movie);
+      this.store.fetchMovies(this.store.urlBaseMovie + this.movie); //recupero i results dei film
+      this.store.fetchTvShows(this.store.urlBaseTvShow + this.movie); //recupero i results delle serie
+      this.store.results = this.store.results.concat(this.store.resultTvShows); //li unisco
+      console.log(this.store.results);
     },
-  },
-  mounted() {
-    this.store.fetchMovies(this.store.urlBase);
   },
 }
 </script>
 
 <template>
-  <h1>Looking for '{{ this.movie }}'...</h1>
+  <h1 class="m-3">Looking for '{{ this.movie }}'...</h1>
 
-  <input v-model="movie" type="text" name="" id="" placeholder="Movies">
+  <input class="m-3" v-model="movie" type="text" name="" id="" placeholder="Movies">
   <button @click="searchMovie()">Search</button>
 
   <ol>
     <li class="my-3" v-for="result in this.store.results">
       <ul>
-        <li><strong>Title: {{ result.title }}</strong></li>
-        <li>Original Title: {{ result.original_title }}</li>
-
+        <li><strong>Title: {{ result.title ? result.title : result.name }}</strong></li>
+        <li v-if="result.original_title || result.original_name">Original Title: {{ result.original_title ?
+          result.original_title : result.original_name }}</li>
         <li>Language: <img
             :src="this.urlFlag + `${result.original_language == 'en' ? 'gb' : result.original_language || result.original_language == 'ja' ? 'jp' : result.original_language}.png`"
             :alt="result.original_language">
