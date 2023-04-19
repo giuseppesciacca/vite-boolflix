615944d5c90b0a734472a6d8cf309a72f837105a
giuseppesciacca <sciaccagi@gmail.com>
2023-04-19 13:27:30 +0200

    [add] flag on language

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,14 @@ export default {
     return {
       store,
       movie: '',
+      urlFlag: 'https://flagcdn.com/20x15/'
     }
   },
   methods: {
     searchMovie() {
-      console.log(this.movie);
+      //console.log(this.movie);
       this.store.fetchMovies(this.store.urlBase + this.movie);
-    }
+    },
   },
   mounted() {
     this.store.fetchMovies(this.store.urlBase);
@@ -22,14 +23,20 @@ export default {
 
 <template>
   <h1>Looking for '{{ this.movie }}'...</h1>
+
   <input v-model="movie" type="text" name="" id="" placeholder="Movies">
   <button @click="searchMovie()">Search</button>
+
   <ol>
     <li class="my-3" v-for="result in this.store.results">
       <ul>
         <li><strong>Title: {{ result.title }}</strong></li>
         <li>Original Title: {{ result.original_title }}</li>
-        <li>Language: {{ result.original_language }}</li>
+
+        <li>Language: <img
+            :src="this.urlFlag + `${result.original_language == 'en' ? 'gb' : result.original_language || result.original_language == 'ja' ? 'jp' : result.original_language}.png`"
+            :alt="result.original_language">
+        </li>
         <li>Vote: {{ result.vote_average }}/10</li>
       </ul>
     </li>
