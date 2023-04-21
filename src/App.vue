d15c1857808b97c8a32134ee0eaae280f19e50c0
giuseppesciacca <sciaccagi@gmail.com>
2023-04-21 12:53:25 +0200

    [add] genre.id on value select

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@ export default {
   data() {
     return {
       store,
-      genreSelected: '',
+      genreSelected: null,
+      urlGenre: 'https://api.themoviedb.org/3/genre/movie/list?api_key=aa7ff67870b167647ae7e4a3d1b611a3',
     }
   },
   components: {
@@ -22,7 +23,7 @@ export default {
     }
   },
   mounted() {
-    this.store.fetchGenres('https://api.themoviedb.org/3/genre/movie/list?api_key=aa7ff67870b167647ae7e4a3d1b611a3')
+    this.store.fetchGenres(this.urlGenre)
   },
 }
 </script>
@@ -31,9 +32,9 @@ export default {
   <AppHeader />
 
   <!-- filter by genre -->
-  <select @change="switchGenre" v-model="genreSelected" class="ms-5" name="" id="">
-    <option disabled value="">Select Genre</option>
-    <option :value="genre.name" v-for="genre in this.store.genres">{{ genre.name }}
+  <select @change="switchGenre" v-model="genreSelected" class="ms-5">
+    <option disabled>Select Genre</option>
+    <option :value="genre.id" v-for="genre in this.store.genres">{{ genre.name }}
     </option>
   </select>
 
